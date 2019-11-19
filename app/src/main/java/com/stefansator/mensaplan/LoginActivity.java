9f1aa56a5cead158f2c32f50dfd3ea9210af1be6
Stefan Sator <stefan.sator@web.de>
2019-11-19 17:53:49 +0100

    Upgraded Components Material Design

--- app/src/main/java/com/stefansator/mensaplan/LoginActivity.java
+++ app/src/main/java/com/stefansator/mensaplan/LoginActivity.java
@@ -1,6 +1,8 @@
 package com.stefansator.mensaplan;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +12,13 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    //Actions
+    // Action for checking login of user and if login correct than logging in the user
+    public void loginButtonClicked(View view) {
+        // TODO
+        Intent intent = new Intent(this.getApplicationContext(), MainActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/stefansator/mensaplan/RegisterActivity.java
+++ app/src/main/java/com/stefansator/mensaplan/RegisterActivity.java
@@ -1,6 +1,8 @@
 package com.stefansator.mensaplan;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +12,13 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+    }
+
+    //Actions
+    //Action to check if Registration is compatible and if compatible send Request for Registration of user to Backend */
+    public void registerButtonClicked(View view) {
+        // TODO
+        Intent intent = new Intent(this.getApplicationContext(), MainActivity.class);
+        startActivity(intent);
     }
 }
